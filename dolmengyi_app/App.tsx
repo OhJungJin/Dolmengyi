9a40f6d36d8b516343763e806b59b98d540ece30
JUNGJIN <ojj824@naver.com>
2024-03-27 20:59:34 +0900

    feat: add webview

--- dolmengyi_app/App.tsx
+++ dolmengyi_app/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <WebView source={{ uri: "https://dolmengyi.vercel.app" }} style={{ flex: 1 }} />
+      <View style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <Text>Open up App.tsx to start working on your app!!!</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
